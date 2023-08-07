56cea564510012aff7cd4896b49eecc9ec15a6e7
Carlos Roberto S Camilo <carlos.rs.camilo@unesp.br>
2023-08-06 21:37:22 -0300

    fix: limites implementados para opção saque

--- src/codigo.py
+++ src/codigo.py
@@ -5,7 +5,7 @@ CLIENTE = {
     'nome': 'Arthur',
     'agencia': 1234,
     'conta': '0001-1',
-    'saldo': 1500,
+    'saldo': 400,
     'limite_diario_saques': 3,
     'limite_valor_por_saque': 500,
     'qtd_saques_dia': 0,
@@ -70,57 +70,65 @@ while True:
 
     if opcao == 2:
         
-        while True:
-            print('Opção selecionada: SAQUE \n')
-            valor = float(input('Informe o valor a ser sacado: '))
+        #Cliente já excedeu o limite de saques no dia?
+        if (CLIENTE['qtd_saques_dia'] >= CLIENTE['limite_diario_saques']):
+            print(f'Limite de saque diário ({CLIENTE["qtd_saques_dia"]}) excedido!')
+            print('Não é possível fazer a operação! Por favor, tente novamente amanhã.')
 
-            if valor <= 0:
-                print('Valor de saque inválido! \n')
-                continue
-            elif valor > 500:
-                print('Valor inválido! O limite de saque por operação é de R$ 500 \n')
-                continue            
-            else:
-                while True:
-                    confirmacao = int(input(f'''
+        else:
+            while True:
+                print('Opção selecionada: SAQUE \n')
+                valor = float(input('Informe o valor a ser sacado: '))
+
+                if valor <= 0:
+                    print('Valor de saque inválido! \n')
+                    continue
+                elif valor > 500:
+                    print('Valor inválido! O limite de saque por operação é de R$ 500 \n')
+                    continue            
+                else:
+                    while True:
+                        confirmacao = int(input(f'''
   Confirma o saque de R$ {valor:.2f}?
 
      [1] Sim
      [0] Não
 
-  '''))
-                    if (confirmacao == 1):
-                        if (CLIENTE['qtd_saques_dia'] <= 3):
-
-                            CLIENTE['saldo'] -= valor
-                            CLIENTE['qtd_saques_dia'] += 1
-                            os.system('clear')
-                            print('Opção selecionada: SIM \n')
-                            print(f'Saque efetuado! \n')
-                            print(f'Novo saldo da conta: R$ {CLIENTE["saldo"]:.2f} \n')
-                            extrato.append(f'Saque...........R$  {valor:0.2f}')
-                        else:
-                            print(f'Limite de saque diário ({CLIENTE["qtd_saques_dia"]}) excedido!')
-                            print('Não é possível fazer a operação! Por favor, tente novamente amanhã.')
+    '''))
+                        if (confirmacao == 1):
                             
-                        break
-                    
-                    elif (confirmacao == 0):
-                        print('\nOpção selecionada: NÃO \n')
-                        break
-                    else:
-                        print('\nOpção inválida! \n')
-                        continue
+                            #O cliente tem saldo suficiente para o saque?
+                            if (CLIENTE['saldo'] - valor) >= 0:
+                                CLIENTE['saldo'] -= valor
+                                CLIENTE['qtd_saques_dia'] += 1
+                                os.system('clear')
+                                print('Opção selecionada: SIM \n')
+                                print(f'Saque efetuado! \n')
+                                print(f'Novo saldo da conta: R$ {CLIENTE["saldo"]:.2f} \n')
+                                extrato.append(f'Saque...........R$  {valor:0.2f}')
+
+                            else:
+                                print('Não é possível efetuar o saque: Saldo insuficiente!')
+                                print(f'Saldo da conta: R$ {CLIENTE["saldo"]:.2f} \n')
+                            break
+                        
+                        elif (confirmacao == 0):
+                            print('\nOpção selecionada: NÃO \n')
+                            break
+                        else:
+                            print('\nOpção inválida! \n')
+                            continue
+                
+                    break
             
-                break
 
     if opcao == 3:
         print('Opção selecionada: EXTRATO \n')
-        print('=== Extrato da Sessão ===\n')
+        print('==== Extrato da Sessão ====\n')
         for item in range(len(extrato)):
             print(extrato[item])
         print(f'Saldo atual:    R$ {CLIENTE["saldo"]:0.2f}\n')
-        print('=========================\n\n')
+        print('===========================\n\n')
 
     if opcao == 0:
         print('\nAgradecemos por utilizar os nossos serviços! Tenha um bom dia! \n')
