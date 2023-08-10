41e46eb32f3ab2669ad1a2e134cc77107702449d
Carlos Roberto S Camilo <carlos.rs.camilo@unesp.br>
2023-08-10 10:51:41 -0300

    fix: aplicada validação no inputs de dados com try/except

--- src/codigo.py
+++ src/codigo.py
@@ -5,7 +5,7 @@ CLIENTE = {
     'nome': 'Arthur',
     'agencia': 1234,
     'conta': '0001-1',
-    'saldo': 1400,
+    'saldo': 400,
     'limite_diario_saques': 3,
     'limite_valor_por_saque': 500,
     'qtd_saques_dia': 0,
@@ -32,27 +32,49 @@ while True:
     '''
     )
 
-    opcao = int(input('Selecione a operação desejada: '))
+    while True:
+        try:
+            opcao = int(input('Selecione a operação desejada: '))
+            break
+        except:
+            print('Opção inválida! \n')
+            continue
+    
     os.system('clear')
 
     if opcao == 1:
         
         while True:
             print('Opção selecionada: DEPÓSITO \n')
-            valor = float(input('Informe o valor a ser depositado: '))
+
+            while True:
+                try:
+                    valor = float(input('Informe o valor a ser depositado: '))
+                    break
+                except:
+                    print('Valor de depósito inválido! \n')
+                    continue
 
             if valor <= 0:
                 print('Valor de depósito inválido! \n')
                 continue
             else:
                 while True:
-                    confirmacao = int(input(f'''
+                    print(f'''
   Confirma o depósito de R$ {valor:.2f}?
 
      [1] Sim
      [0] Não
 
-  '''))
+  ''')
+                    while True:
+                        try:
+                            confirmacao = int(input('Confirmação: '))
+                            break
+                        except:
+                            print('Opção inválida! \n')
+                            continue
+
                     if (confirmacao == 1):
                         CLIENTE['saldo'] += valor
                         os.system('clear')
@@ -81,8 +103,15 @@ while True:
         else:
             while True:
                 print('Opção selecionada: SAQUE \n')
-                valor = float(input('Informe o valor a ser sacado: '))
 
+                while True:
+                    try:
+                        valor = float(input('Informe o valor a ser sacado: '))
+                        break
+                    except:
+                        print('Valor de saque inválido! \n')
+                        continue
+                
                 if valor <= 0:
                     print('Valor de saque inválido! \n')
                     continue
@@ -91,13 +120,21 @@ while True:
                     continue            
                 else:
                     while True:
-                        confirmacao = int(input(f'''
+                        print(f'''
   Confirma o saque de R$ {valor:.2f}?
 
      [1] Sim
      [0] Não
 
-    '''))
+    ''')
+                        while True:
+                            try:
+                                confirmacao = int(input('Confirmação: '))
+                                break
+                            except:
+                                print('Opção inválida! \n')
+                                continue
+
                         if (confirmacao == 1):
                             
                             #O cliente tem saldo suficiente para o saque?
@@ -108,11 +145,13 @@ while True:
                                 print('Opção selecionada: SIM \n')
                                 print(f'Saque efetuado! \n')
                                 print(f'Novo saldo da conta: R$ {CLIENTE["saldo"]:.2f} \n')
+                                input('Pressione qualquer tecla para continuar.')
                                 extrato.append(f'Saque...........R$  {valor:0.2f}')
 
                             else:
-                                print('Não é possível efetuar o saque: Saldo insuficiente!')
+                                print('\nNão é possível efetuar o saque: Saldo insuficiente!')
                                 print(f'Saldo da conta: R$ {CLIENTE["saldo"]:.2f} \n')
+                                input('Pressione qualquer tecla para continuar.')
                             break
                         
                         elif (confirmacao == 0):
@@ -139,10 +178,11 @@ while True:
                 print(extrato[item])
             print(f'Saldo atual:    R$ {CLIENTE["saldo"]:0.2f}\n')
         print('===========================\n\n')
+        input('Pressione qualquer tecla para continuar.')
 
     if opcao == 0:
         print('\nAgradecemos por utilizar os nossos serviços! Tenha um bom dia! \n')
-        print('=== Sessão Encerrada === = \n')
+        print('=== Sessão Encerrada === \n')
         break
 
     print('\nDeseja realizar outra operação?\n')
